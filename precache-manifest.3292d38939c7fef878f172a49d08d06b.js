f660abbe957bb5652b79d791534dc4019ba0ffdd
anas <anas3506606@gmail.com>
2020-07-13 21:10:26 +0300

    Updates

---
+++ precache-manifest.3292d38939c7fef878f172a49d08d06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8118fab8d2b65165811fd175ad21284",
+    "url": "/clone-raeact/index.html"
+  },
+  {
+    "revision": "4ca4835262653dc5615a",
+    "url": "/clone-raeact/static/css/main.fb01fccb.chunk.css"
+  },
+  {
+    "revision": "6d7fae9b1db49a00a9c2",
+    "url": "/clone-raeact/static/js/2.4c1e4794.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clone-raeact/static/js/2.4c1e4794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca4835262653dc5615a",
+    "url": "/clone-raeact/static/js/main.6c2d2fb4.chunk.js"
+  },
+  {
+    "revision": "8ce4c354f9853c283359",
+    "url": "/clone-raeact/static/js/runtime-main.ab1e9277.js"
+  }
+]);
